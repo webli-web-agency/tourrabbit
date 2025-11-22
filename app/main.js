2fe8b0fdf4e117a557bdfc37d2d0859a71875c89
Rishabh Srivastava <104848594+rishabh0777@users.noreply.github.com>
2025-11-22 14:56:45 +0000

    fix: uncomment SmoothScroll component in Main and remove "use client" directive from SmoothScroll

--- app/main.js
+++ app/main.js
@@ -9,11 +9,11 @@ import TestimonialsMain from "../components/testimonials/index.js";
 import PackagesMain from "../components/packages/index.js";
 import ServicesMain from "../components/services/index.js";
 
-// import SmoothScroll from "../components/smoothScroll.js"; 
+import SmoothScroll from "../components/smoothScroll.js"; 
 
 const Main = () => {
   return (
-
+    <SmoothScroll>
       <main className="relative w-screen overflow-x-hidden overflow-y-visible">
         <Header />
         <HomeMain />
@@ -24,7 +24,7 @@ const Main = () => {
         <ContactMain />
         <Footer />
       </main>
-
+    </SmoothScroll>
   );
 };
 
